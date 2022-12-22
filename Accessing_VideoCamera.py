90b86d7ee665df64441e0294b3507cb2613b351e
GO V IND <govind123.ga@gmail.com>
2022-12-22 23:10:18 +0530

    Update Accessing_VideoCamera.py

--- Accessing_VideoCamera.py
+++ Accessing_VideoCamera.py
@@ -4,7 +4,7 @@ video = cv2.VideoCapture(0)
 
 while (True):
 
-    ret, frame = video.read()
+    success, frame = video.read()
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
